74a410624dda4307bac6ecddd3b7a326330e10db
Eder <eder.padilla97@gmail.com>
2020-03-24 15:50:37 -0600

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Eder  Padilla on 24/03/20.
+//  Copyright © 2020 EderPadilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
