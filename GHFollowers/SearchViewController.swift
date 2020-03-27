359eff39ab0d672e0f2d36bd9c15a2de70fbd597
Eder <eder.padilla97@gmail.com>
2020-03-27 17:46:12 -0600

    Add logo imageview

--- GHFollowers/SearchViewController.swift
+++ GHFollowers/SearchViewController.swift
@@ -2,8 +2,31 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    let logoImageView = UIImageView()
+    let userNameTextField = PrimaryTextField()
+    let goButton = PrimaryButton(backgroundColor: .systemGreen, title: "Get Followers")
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        setUpLogoImageView()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+
+    private func setUpLogoImageView() {
+        view.addSubview(logoImageView)
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
     }
 }
