3668459e0ee3e71fe4ed79beeb8a6ffd6305256b
Eder <eder.padilla97@gmail.com>
2020-03-28 17:46:31 -0600

    Create vody label

---
+++ GHFollowers/BodyLabel.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class BodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        setUp()
+    }
+
+    private func setUp() {
+        translatesAutoresizingMaskIntoConstraints = false
+        font = UIFont.preferredFont(forTextStyle: .body)
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+    }
+}
