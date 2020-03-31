cfe7fdcd6c47c5864b39d375045dc3ee2a4b8a9e
Eder <eder.padilla97@gmail.com>
2020-03-31 12:51:00 -0600

    Refactor code style and add collection view cell

--- GHFollowers/ui/FollowersViewController.swift
+++ GHFollowers/ui/FollowersViewController.swift
@@ -4,13 +4,34 @@ class FollowersViewController: UIViewController {
 
     var userName: String!
 
+    var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpViewController()
+        setUpCollectionView()
+        getFollowers(userName: self.userName)
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
+    private func setUpViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
 
+    private func setUpCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseId)
+    }
+
+    private func getFollowers(userName: String) {
         NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
-
             switch result {
 
             case .success(let followers):
@@ -21,10 +42,5 @@ class FollowersViewController: UIViewController {
 
             }
         }
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }
