b333b724058d6680079f07609177b08777148527
Eder <eder.padilla97@gmail.com>
2020-03-31 19:01:31 -0600

    Create empty state

---
+++ GHFollowers/views/views/EmptyStateView.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+class EmptyStateView: UIView {
+
+    private let messageLabel = PrimaryTitleLabel(textAlignment: .center, fontSize: 28)
+    private let logoImageView = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    init(message : String) {
+        super.init(frame: .zero)
+        messageLabel.text = message
+        setUp()
+    }
+
+    private func setUp() {
+        addSubview(messageLabel)
+        addSubview(logoImageView)
+
+        messageLabel.numberOfLines = 3
+        messageLabel.textColor = .secondaryLabel
+
+        logoImageView.image = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200),
+
+            logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 200),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 140)
+        ])
+    }
+}
