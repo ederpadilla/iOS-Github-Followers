4fdfcf6fc117ba80b9e95e040d188ac0a10340df
Eder <eder.padilla97@gmail.com>
2020-03-31 19:10:50 -0600

    Move image

--- GHFollowers/views/views/EmptyStateView.swift
+++ GHFollowers/views/views/EmptyStateView.swift
@@ -38,8 +38,8 @@ class EmptyStateView: UIView {
 
             logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
             logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1.3),
-            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 200),
-            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 140)
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 170),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 40)
         ])
     }
 }
