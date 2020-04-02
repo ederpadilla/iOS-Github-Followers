045c245d5fa6352fb8dea2a069d64fc5dca9231c
Eder <eder.padilla97@gmail.com>
2020-04-02 14:31:56 -0600

    Create item info type

--- GHFollowers/ui/UserInfoViewController.swift
+++ GHFollowers/ui/UserInfoViewController.swift
@@ -2,29 +2,25 @@ import UIKit
 
 class UserInfoViewController: UIViewController {
 
-    let headerView  = UIView()
+    let headerView = UIView()
+    let itemViewOne = UIView()
+    let itemViewTwo = UIView()
+
+    var itemViews : [UIView] = []
 
     var username: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        setUpViewController()
         layoutUI()
+        getUserInfo()
+    }
+
+    private func setUpViewController() {
+        view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissViewController))
         navigationItem.rightBarButtonItem = doneButton
-        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
-            guard let self = self else { return }
-            
-            switch result {
-            case .success(let user):
-                DispatchQueue.main.async {
-                    self.addChildViewController(childViewController: UserInfoHeaderViewController(user: user), containerView: self.headerView)
-                }
-                
-            case .failure(let error):
-                self.presentCustomAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonText: "Ok")
-            }
-        }
     }
 
     @objc private func dismissViewController() {
@@ -32,22 +28,56 @@ class UserInfoViewController: UIViewController {
     }
 
     func layoutUI() {
-        view.addSubview(headerView)
-        headerView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        let itemHeight: CGFloat = 140
+
+        itemViews = [headerView, itemViewOne, itemViewTwo]
+
+        itemViewOne.backgroundColor = .systemPink
+        itemViewTwo.backgroundColor = .systemGreen
+
+        for itemView in  itemViews {
+            view.addSubview(itemView)
+            itemView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+            ])
+        }
 
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
+            headerView.heightAnchor.constraint(equalToConstant: 180),
+
+            itemViewOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            itemViewOne.heightAnchor.constraint(equalToConstant: itemHeight),
+
+            itemViewTwo.topAnchor.constraint(equalTo: itemViewOne.bottomAnchor, constant: padding),
+            itemViewTwo.heightAnchor.constraint(equalToConstant: itemHeight)
         ])
     }
 
-    
-    func addChildViewController(childViewController: UIViewController,containerView: UIView) {
+
+    func addChildViewController(childViewController: UIViewController, containerView: UIView) {
         addChild(childViewController)
         containerView.addSubview(childViewController.view)
         childViewController.view.frame = containerView.bounds
         childViewController.didMove(toParent: self)
+    }
+
+    private func getUserInfo() {
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+
+            switch result {
+            case .success(let user):
+                DispatchQueue.main.async {
+                    self.addChildViewController(childViewController: UserInfoHeaderViewController(user: user), containerView: self.headerView)
+                }
+
+            case .failure(let error):
+                self.presentCustomAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonText: "Ok")
+            }
+        }
     }
 }
--- GHFollowers/utilities/Constants.swift
+++ GHFollowers/utilities/Constants.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 enum SFSymbols {
-    static let location = "mappin.and.ellipse"
+    static let location     = "mappin.and.ellipse"
+    static let repos        = "folder"
+    static let gists        = "text.alignleft"
+    static let followers    = "heart"
+    static let following    = "person.2"
 }
