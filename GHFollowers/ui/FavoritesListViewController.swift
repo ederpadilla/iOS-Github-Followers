306a671190d869be1804a495d9f6b43697b097b4
Eder <eder.padilla97@gmail.com>
2020-04-03 12:44:17 -0600

    Retrieve persistence followers 💣

--- GHFollowers/ui/FavoritesListViewController.swift
+++ GHFollowers/ui/FavoritesListViewController.swift
@@ -5,5 +5,18 @@ class FavoritesListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
+        getFavoritesListSaved()
+    }
+
+    private func getFavoritesListSaved() {
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print("favorites 🔥 \(favorites)")
+                break
+            case .failure(let error):
+                break
+            }
+        }
     }
 }
