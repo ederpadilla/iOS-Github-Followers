e0e7f7c24edf54e8b3b73d51026f69e2ed8556d4
Eder <eder.padilla97@gmail.com>
2020-04-03 14:05:57 -0600

    add delete function on swipe left 🏄🏻

--- GHFollowers/ui/FavoritesListViewController.swift
+++ GHFollowers/ui/FavoritesListViewController.swift
@@ -76,4 +76,18 @@ extension FavoritesListViewController: UITableViewDataSource, UITableViewDelegat
 
         navigationController?.pushViewController(followerListViewController, animated: true)
     }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            guard let error = error else { return }
+            self.presentCustomAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonText: "Ok")
+        }
+    }
 }
