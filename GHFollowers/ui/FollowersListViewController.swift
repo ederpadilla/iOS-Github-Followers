611010260e9637fe7ed90ae561cc3cd7a9084b47
Eder <eder.padilla97@gmail.com>
2020-04-03 19:35:39 -0600

    better scroll animation 🏄‍

--- GHFollowers/ui/FollowersListViewController.swift
+++ GHFollowers/ui/FollowersListViewController.swift
@@ -185,7 +185,7 @@ extension FollowersListViewController: FollowerListViewControllerDelegate {
         page = 1
         followers.removeAll()
         filteredFollowers.removeAll()
-        collectionView.setContentOffset(.zero, animated: true)
+        collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: true)
         getFollowers(userName: username, page: page)
     }
 }
