943c5ee34af53aada533f872e9c55d33bdda556d
Eder <eder.padilla97@gmail.com>
2020-04-05 13:18:47 -0500

    Kill potential bug 🐛

--- GHFollowers/ui/FavoritesListViewController.swift
+++ GHFollowers/ui/FavoritesListViewController.swift
@@ -81,13 +81,13 @@ extension FavoritesListViewController: UITableViewDataSource, UITableViewDelegat
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         guard editingStyle == .delete else { return }
 
-        let favorite = favorites[indexPath.row]
-        favorites.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .left)
-
-        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+        PersistenceManager.updateWith(favorite: favorites[indexPath.row], actionType: .remove) { [weak self] error in
             guard let self = self else { return }
-            guard let error = error else { return }
+            guard let error = error else {
+                self.favorites.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .left)
+                return
+            }
             self.presentCustomAlertOnMainThread(title: "Unable to remove 😅", message: error.rawValue, buttonText: "Ok")
         }
     }
